7dc7c2b9db75a4a3efe30f25e7ac38e39c0d251b
Rinal <rinalwadekar9196@gmail.com>
2024-02-13 00:28:47 +0530

    did dice images change on click of roll buttons for both players

--- 2nd_class/scripts/game.js
+++ 2nd_class/scripts/game.js
@@ -1,22 +1,49 @@
-const rolldice1 =  document.getElementById("rolldice1");
-const rolldice2 =  document.getElementById("rolldice2");
-const score1 = document.getElementById("score1");
-const score2 = document.getElementById("score2");
+const score0E1 = document.getElementById("score1");
+const score1E1 = document.getElementById("score2");
 
-const min = 1;
-const max = 6;
-let randomNum;
+const diceE1 = document.querySelector(".dice");
+const diceE2 = document.querySelector(".dice2");
+const btnRoll = document.querySelector(".diceButton");
+const btnRoll2 = document.querySelector(".diceButton2");
 
-rolldice1.onclick = function(){
+score0E1.textContent = 0;
+score1E1.textContent = 0;
 
-    randomNum = Math.floor(Math.random() * max) + min;
-    score1.textContent = randomNum;
+const current0E1 =  document.getElementById("current_1");
+const current0E2 =  document.getElementById("current_2");
 
-}
+diceE1.classList.add("hidden");
 
-rolldice2.onclick = function(){
+let activePlayer = 0;
+let currentScore = 0;
 
-    randomNum = Math.floor(Math.random() * max) + min;
-    score2.textContent = randomNum;
+btnRoll.addEventListener("click", function() {
+    diceE1.classList.remove("hidden"); 
 
-}
+    const dice = Math.floor(Math.random()*6) +1;
+    // console.log(dice);
+    diceE1.src = `/assets/dice-${dice}.png`;
+
+    if (dice !== 1){
+        currentScore  +=  dice;
+        // current0E1.textContent = currentScore;
+        document.getElementById(`current_$ {activePlayer}`).textContent = currentScore;
+    }else{
+        document.getElementById(`current_$ {activePlayer}`).textContent = 0;
+    }
+});
+
+diceE2.classList.add("hidden");
+
+btnRoll2.addEventListener("click", function() {
+    diceE2.classList.remove("hidden"); 
+
+    const dice2= Math.floor(Math.random()*6) +1;
+    // console.log(dice2);
+    diceE2.src = `/assets/dice-${dice2}.png`;
+});
+
+
+
+
+
---
+++ 2nd_class/scripts/third_lec.js
@@ -0,0 +1,5 @@
+/*Pollyfills*/ 
+
+let array = [12 ,22, 30, 412, 50]
+
+Array.prototype.customForeach = 
